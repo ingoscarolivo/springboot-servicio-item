2499e03bd007ac21855376651cceb4615716f376
Oscar Olivo <oscar.olivo@ceiba.com.co>
2023-02-27 23:51:38 -0500

    Timeout

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -36,7 +36,7 @@ public class ItemController {
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return cbFactory.create("items")
-				.run(() -> itemService.findById(id, cantidad)/*,e -> metodoAlternativo(id, cantidad, e)*/);
+				.run(() -> itemService.findById(id, cantidad),e -> metodoAlternativo(id, cantidad, e));
 	}
 
 
