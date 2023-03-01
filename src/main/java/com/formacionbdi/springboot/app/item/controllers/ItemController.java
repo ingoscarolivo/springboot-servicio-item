1022cd1e7dd2acb894707a11472042947ab00ac4
Oscar Olivo <oscar.olivo@ceiba.com.co>
2023-02-28 23:33:01 -0500

    actualizando cambio en configuraciones con @RefreshScope y actuator

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,8 @@ import org.springframework.web.bind.annotation.*;
 import com.formacionbdi.springboot.app.item.models.Item;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
 
+
+@RefreshScope
 @RestController
 public class ItemController {
 
